5ea813782e3d57308999867bc51b9c14fc05af44
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-25 16:01:25 +0900

    move method Customer.amout_for to rental.charge

--- sec1/rental.rb
+++ sec1/rental.rb
@@ -8,4 +8,18 @@ class Rental
     
   end
   
+  def charge
+    result = 0
+    case @movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (@days_rented - 2) * 1.5 if @days_rented > 2
+    when Movie::NEW_RELEASE
+      result += @days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (@days_rented - 3) * 1.5 if @days_rented > 3
+    end
+    result
+  end
 end
