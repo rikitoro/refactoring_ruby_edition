c81cba9d03a45bd1e22ab620b1b1510265378125
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-25 14:59:40 +0900

    sec1 initial version

---
+++ sec1/main.rb
@@ -0,0 +1,13 @@
+require "./customer"
+
+gone_with_window = Movie.new("Gone with Window", Movie::REGULAR)
+the_gold_rush = Movie.new("The Gold Rush", Movie::NEW_RELEASE)
+
+rental1 = Rental.new(the_gold_rush, 5)
+rental2 = Rental.new(gone_with_window, 4)
+
+yoshiaki = Customer.new("yoshiaki")
+yoshiaki.add_rental(rental1)
+yoshiaki.add_rental(rental2)
+
+puts yoshiaki.statement
