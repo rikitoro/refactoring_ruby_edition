55c5639dfa1a0740212d7a46e404e8f53075adc4
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-26 11:56:53 +0900

    extract Customer.total_frequent_renter_points

--- sec1/customer.rb
+++ sec1/customer.rb
@@ -16,20 +16,21 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-
-      frequent_renter_points += element.frequent_renter_points
-
       result += "\t" + element.movie.title + 
                 "\t" + element.charge.to_s + "\n"
-      total_amount += element.charge      
-
     end
         
-    result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "Amount owed is #{total_charge}\n"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
+  def total_charge
+    @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
 
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
+  end
 
 end
