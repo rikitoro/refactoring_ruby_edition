329caf3ea4a139482a7e192bc55f2b84a34b4fb4
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-26 16:48:23 +0900

    replace Type code with state for Movie.frequent_renter_points to Price.frequent_renter_points

--- sec1/movie.rb
+++ sec1/movie.rb
@@ -6,19 +6,10 @@ class Movie
   CHILDRENS = 2
 
   attr_reader :title
-  attr_accessor :price_code
+  attr_writer :price
 
-  def price_code=(value)
-    @price_code = value
-    @price = case @price_code
-      when REGULAR; RegularPrice.new
-      when NEW_RELEASE; NewReleasePrice.new
-      when CHILDRENS; ChildrensPrice.new
-    end   
-  end
-
-  def initialize(title, the_price_code)
-    @title, self.price_code = title, the_price_code 
+  def initialize(title, price)
+    @title, @price = title, price 
   end
 
   def charge(days_rented)
@@ -26,6 +17,6 @@ class Movie
   end
 
   def frequent_renter_points(days_rented)
-      (price_code == Movie::NEW_RELEASE &&  days_rented > 1) ? 2 : 1        
+    @price.frequent_renter_points(days_rented)
   end
 end
--- sec1/price.rb
+++ sec1/price.rb
@@ -1,4 +1,11 @@
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end 
+
 class RegularPrice
+  include DefaultPrice
   def charge(days_rented)
     result = 2
     result += (days_rented - 2) * 1.5 if days_rented > 2
@@ -7,12 +14,17 @@ class RegularPrice
 end
 
 class NewReleasePrice
+  def frequent_renter_points(days_rented)
+    days_rented > 2 ? 2 : 1  
+  end
+
   def charge(days_rented)
     days_rented * 3
   end
 end
 
 class ChildrensPrice
+  include DefaultPrice
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
