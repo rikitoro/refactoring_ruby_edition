3449f80a37cb8e8a8e0b1eaf43735d6a671583d2
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-26 18:09:38 +0900

    add customer_spec.rb

--- sec1/customer.rb
+++ sec1/customer.rb
@@ -13,8 +13,33 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
+    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
+    @rentals.each do |element|      
+      this_amount = 0
+
+      case element.movie.price_code
+      when Movie::REGULAR
+        this_amount += 2
+        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+      when Movie::NEW_RELEASE
+        this_amount += element.days_rented * 3
+      when Movie::CHILDRENS
+        this_amount += 1.5
+        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+      end
+
+      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+
+    end
+        
+    result += "Amount owed is #{total_amount}\n"
+    result += "You earned #{self.frequent_renter_points} frequent renter points"
+    result
+  end
+
+  def total_amount
+    result = 0
     @rentals.each do |element|
       this_amount = 0
 
@@ -29,19 +54,21 @@ class Customer
         this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
       end
 
-      frequent_renter_points += 1
-
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
-
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount      
-
+      result += this_amount      
     end
         
-    result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
+
+  def frequent_renter_points
+    result = 0
+    @rentals.each do |element|      
+      result += 1
+      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+        result += 1
+      end
+    end
+    result
+  end
+
 end
---
+++ sec1/customer_spec.rb
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'rspec'
+require_relative 'customer'
+
+describe Customer do
+  before do
+    @gone_with_window = Movie.new("Gone with Window", Movie::REGULAR)
+    @rental_gww = Rental.new(@gone_with_window, 5)
+    @the_gold_rush = Movie.new("The Gold Rush", Movie::NEW_RELEASE)
+    @rental_tgr = Rental.new(@the_gold_rush, 5)
+    @yoshiaki = Customer.new("yoshiaki")
+  end
+
+  context '1 REGULAR rental with 5 rented days:' do
+    before { @yoshiaki.add_rental(@rental_gww) }
+
+    describe '#total_amount' do
+      subject { @yoshiaki.total_amount }
+      it { should == 6.5 }
+    end
+
+    describe '#frequent_renter_points' do
+      subject { @yoshiaki.frequent_renter_points }
+      it { should == 1 }
+    end
+  end
+
+  context '1 REGULAR and 1 NEW_RELEASE rentals with 5 rented days:' do 
+    before do
+      @yoshiaki.add_rental(@rental_gww) 
+      @yoshiaki.add_rental(@rental_tgr)
+    end
+
+    describe '#total_amount' do
+      subject { @yoshiaki.total_amount }
+      it { should == 21.5 }
+    end
+
+    describe '#frequent_renter_points' do
+      subject { @yoshiaki.frequent_renter_points }
+      it { should == 3 }
+    end
+  end
+
+end
