baf1e657eadeb3805261fb996d6316798c704da4
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-26 12:24:14 +0900

    move Rental.frequent_renter_points to Movie.frequent_renter_points(days_rented)

--- sec1/rental.rb
+++ sec1/rental.rb
@@ -14,7 +14,6 @@ class Rental
 
 
   def frequent_renter_points
-    (@movie.price_code == Movie::NEW_RELEASE && 
-      @days_rented > 1) ? 2 : 1    
+    @movie.frequent_renter_points(@days_rented)
   end
 end
