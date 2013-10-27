1377e0f3796376d3e1a881eb35405de65ed0d261
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-27 15:24:23 +0900

    move Rental.frequent_renter_points to Movie.frequent_renter_points(days_rented)

--- sec1/movie.rb
+++ sec1/movie.rb
@@ -26,5 +26,9 @@ class Movie
     @price.charge(days_rented)
   end
 
+  def frequent_renter_points(days_rented)
+    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1   
+  end
+
 end
 
--- sec1/movie_spec.rb
+++ sec1/movie_spec.rb
@@ -9,10 +9,15 @@ describe Movie do
   context 'REGULAR movie with 5 rented days:' do
     before { @gone_with_window = Movie.new("Gone with Window", Movie::REGULAR) }
 
-    describe '#total_charge(rented_days)' do
+    describe '#charge(rented_days)' do
       subject { @gone_with_window.charge(5) }
       it { should == 6.5 }
     end
+
+    describe '#frequent_renter_points(rented_days)' do
+      subject { @gone_with_window.frequent_renter_points(5) }
+      it { should == 1 }
+    end  
   end
 
   context 'NEW_RELEASE movie with 4 rented days:' do
@@ -22,6 +27,13 @@ describe Movie do
       subject { @the_gold_rush.charge(4) }
       it { should == 12 }
     end
+
+    describe '#frequent_renter_points(rented_days)' do
+      subject { @the_gold_rush.frequent_renter_points(4) }
+      it { should == 2 }
+    end  
+
+
   end
 
 end
