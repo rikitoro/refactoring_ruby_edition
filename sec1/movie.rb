5e23fe1df67ecd46325ebfdb80c327113dfbb105
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-27 15:14:46 +0900

    replace type code in Movie.charge with State *Price.charge

--- sec1/movie.rb
+++ sec1/movie.rb
@@ -1,28 +1,30 @@
+require_relative 'price'
+
 class Movie
   REGULAR = 0
   NEW_RELEASE = 1
   CHILDRENS = 2
 
   attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code
+
+  def price_code=(code)
+    @price_code = code
+    @price = case @price_code
+      when REGULAR; RegularPrice.new
+      when NEW_RELEASE; NewReleasePrice.new
+      when CHILDRENS; ChildrensPrice.new
+    end
+  end
 
   def initialize(title, price_code)
-    @title, @price_code = title, price_code 
+    @title = title
+    self.price_code = price_code 
   end
 
   def charge(days_rented)
-    result = 0
-    case @price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result      
+    @price.charge(days_rented)
   end
-  
+
 end
+
