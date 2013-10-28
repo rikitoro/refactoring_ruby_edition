86af8ab05b806184cafe63499441ff57dc418864
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-28 19:16:19 +0900

    replace Select#add_option(option) with chain

--- sec06_05/select.rb
+++ sec06_05/select.rb
@@ -1,9 +1,17 @@
 class Select
+
   def options
     @options ||= []
   end
 
+  def self.with_option(option)
+    select = self.new
+    select.options << option
+    select
+  end
+
   def add_option(arg)
     options << arg
+    self
   end
 end
--- sec06_05/select_spec.rb
+++ sec06_05/select_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require_relative 'select'
 
 describe Select do
+
   describe '#options' do
     context 'when add options 1999, 2000, 2001' do
       before do
@@ -13,5 +14,22 @@ describe Select do
       subject { @select.options }
       it { should =~ [1999, 2000, 2001] }
     end
+    context 'when add options 1999, 2000, 2001 by chain' do
+      before do
+        @select = Select.new
+        @select.add_option(1999).add_option(2000).add_option(2001)
+      end
+      subject { @select.options }
+      it { should =~ [1999, 2000, 2001] }
+    end
+    context 'when with_option(100)' do
+      before do
+        @select = Select.with_option(100)
+      end
+      subject { @select.options }
+      it { should =~ [100] }
+    end
+
+
   end
 end
