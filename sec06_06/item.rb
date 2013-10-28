a82451997afd868abc891c75007b428e5b2c39b0
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-28 20:03:24 +0900

    introduce explaining variable in Item#price

--- sec06_06/item.rb
+++ sec06_06/item.rb
@@ -5,8 +5,9 @@ class Item
   end
   
   def price
-    return @quantity * @item_price -
-      [0, @quantity - 500].max * @item_price * 0.05 +
-      [@quantity * @item_price * 0.1, 100.0].min    
+    base_price = @quantity * @item_price
+    quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
+    shipping = [base_price * 0.1, 100.0].min    
+    return base_price - quantity_discount + shipping
   end
 end
