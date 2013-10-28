5befb9068c120fa5fcddce7cf319bdaaeba813e7
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-28 18:48:24 +0900

    sec06_04

---
+++ sec06_04/item.rb
@@ -0,0 +1,18 @@
+class Item
+  def initialize(quantity, item_price)
+    @quantity = quantity
+    @item_price = item_price
+  end
+
+  def price
+    base_price * discount_factor
+  end
+
+  def base_price
+    @quantity * @item_price
+  end
+
+  def discount_factor
+    (base_price > 1000) ? 0.95 : 0.98
+  end
+end
