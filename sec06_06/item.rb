95d2d94cebad1592eb640b6cfdff5905e590487f
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-10-28 20:06:46 +0900

    extract Item#base_price

--- sec06_06/item.rb
+++ sec06_06/item.rb
@@ -5,9 +5,12 @@ class Item
   end
   
   def price
-    base_price = @quantity * @item_price
     quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
     shipping = [base_price * 0.1, 100.0].min    
     return base_price - quantity_discount + shipping
+  end
+
+  def base_price
+    @quantity * @item_price
   end
 end
