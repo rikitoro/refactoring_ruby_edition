ab252f39bd0c16eb68ce53de03896a42725e7dc2
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-04 13:57:57 +0900

    refactor specs

--- sec06_09/account_spec.rb
+++ sec06_09/account_spec.rb
@@ -3,10 +3,9 @@ require_relative 'account'
 
 describe Account do
   before { @account = Account.new }
-  describe '#gamma' do
-    context 'when input_val = 123, quantity = 34, year_to_date = 365' do
-      subject { @account.gamma(123, 34, 365) }
-      it { should  == 306401 }
-    end
+  describe '#gamma', 'with args input_val = 123, quantity = 34, year_to_date = 365' do
+    subject { @account.gamma(123, 34, 365) }
+    it { should  == 306401 }
+    it { should_not == 122 }
   end 
 end
