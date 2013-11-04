f2214bf65ed9ffcead8fdf93e232f027daa0460e
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-04 14:40:00 +0900

    add Ledger and its specs

---
+++ sec06_08/ledger.rb
@@ -0,0 +1,11 @@
+class Ledger
+  attr_reader :balance
+
+  def initialize(balance)
+    @balance = balance
+  end
+
+  def add(arg)
+    @balance += arg
+  end
+end
---
+++ sec06_08/triple.rb
@@ -0,0 +1,7 @@
+x = 5
+def triple(arg)  
+  arg = arg * 3
+  puts "arg in triple: #{arg}"
+end
+triple x
+puts "x after triple #{x}"
