d3c919ef26638b76302d12ee3cbd6edc0b08fae7
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-05 13:13:37 +0900

    introduce Class Annotation hash_initializer

--- sec06_13/search_criteria.rb
+++ sec06_13/search_criteria.rb
@@ -1,12 +1,14 @@
 class SearchCriteria
   attr_reader :author_id, :publisher_id, :isbn
 
-  def initialize(hash)
-    @author_id = hash[:author_id]
-    @publisher_id = hash[:publisher_id]
-    @isbn = hash[:isbn]
+  def self.hash_initialier(*attribute_names)
+    define_method(:initialize) do |*args|
+      data = args.first || {}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "@#{attribute_name}", data[attribute_name]
+      end
+    end
   end
-
-
   
+  hash_initialier :author_id, :publisher_id, :isbn
 end
