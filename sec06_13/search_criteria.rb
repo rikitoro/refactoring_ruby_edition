fd195e6da952103170b2f245cc32b708b8694ee1
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-05 12:45:39 +0900

    fake it, SearchCriteria

--- sec06_13/search_criteria.rb
+++ sec06_13/search_criteria.rb
@@ -1,10 +1,12 @@
 class SearchCriteria
+  attr_reader :author_id, :publisher_id, :isbn
+
   def initialize(hash)
-    
+    @author_id = 1  
+    @publisher_id = 3
+    @isbn = "1234567890"
   end
 
-  def author_id
-    1
-  end
+
   
 end
--- sec06_13/search_criteria_spec.rb
+++ sec06_13/search_criteria_spec.rb
@@ -2,13 +2,19 @@ require 'rspec'
 require_relative 'search_criteria'
 
 describe SearchCriteria do 
-  context 'author_id = 1, publisher_id = 3, isbn = "1234567890"' do
+  context 'with author_id = 1, publisher_id = 3, isbn = "1234567890"' do
     before {
       @sc = SearchCriteria.
       new({author_id: 1, publisher_id: 3, isbn: "1234567890"})
     }
     describe '#author_id' do
       it { expect(@sc.author_id).to eq 1 }
+    end
+    describe '#publisher_id' do
+      it { expect(@sc.publisher_id).to eq 3 }
+    end
+    describe '#isbn' do
+      it { expect(@sc.isbn).to eq "1234567890" }
     end
   end
 end
