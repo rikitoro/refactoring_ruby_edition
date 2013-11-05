b1d6036c1b1cec3dc8a1109594592d61ad6f1e1a
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-05 13:24:16 +0900

    move CustomInitializers module to custom_initializers.rb

---
+++ sec06_13/custom_initializers.rb
@@ -0,0 +1,14 @@
+module CustomInitializers
+
+  def hash_initializer(*attribute_names)
+    define_method(:initialize) do |*args|
+      data = args.first || {}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "@#{attribute_name}", data[attribute_name]
+      end
+    end
+  end
+
+end
+
+Class.send :include, CustomInitializers
--- sec06_13/search_criteria.rb
+++ sec06_13/search_criteria.rb
@@ -1,17 +1,4 @@
-module CustomInitializers
-
-  def hash_initializer(*attribute_names)
-    define_method(:initialize) do |*args|
-      data = args.first || {}
-      attribute_names.each do |attribute_name|
-        instance_variable_set "@#{attribute_name}", data[attribute_name]
-      end
-    end
-  end
-
-end
-
-Class.send :include, CustomInitializers
+require_relative 'custom_initializers'
 
 class SearchCriteria
   attr_reader :author_id, :publisher_id, :isbn
