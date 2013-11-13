38808a2833c6c0401e6a8bc9fc8366625ce4d532
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-13 22:17:11 +0900

    introduce named parameter

--- sec06_14/books.rb
+++ sec06_14/books.rb
@@ -1,12 +1,15 @@
 class Books
-  def self.find(selector, conditions = "", *joins)
+  def self.find(selector, hash={})
+    hash[:joins] ||= []
+    hash[:conditions] ||= ""
+
     sql = ["SELECT * FROM books"]
-    joins.each do |join_table|
+    hash[:joins].each do |join_table|
       sql << "LEFT OUTER JOIN #{join_table} ON"
       sql << "books.#{join_table.to_s.chop}_id"
       sql << " = #{join_table}.id"
     end
-    sql << "WHERE #{conditions}" unless conditions.empty?
+    sql << "WHERE #{hash[:conditions]}" unless hash[:conditions].empty?
     sql << "LIMIT 1" if selector == :first
 
     connection_find(sql.join(" "))
--- sec06_14/books_spec.rb
+++ sec06_14/books_spec.rb
@@ -11,16 +11,18 @@ describe Books do
         to eq "SELECT * FROM books"
     end
     it "with 1st arg :all, conditions for 'title'" do
-      expect(Books.find(:all, "title like '%Voodoo Economics'")).
+      expect(Books.find(:all, conditions: "title like '%Voodoo Economics'")).
         to eq "SELECT * FROM books WHERE title like '%Voodoo Economics'"     
     end
     it "with 1st arg :all, conditions for 'authors.name', joins :authors" do
-      expect(Books.find(:all, "authors.name = 'Jenny James'", :authors)).
+      expect(Books.
+        find(:all, conditions: "authors.name = 'Jenny James'", joins: [:authors])).
         to eq "SELECT * FROM books LEFT OUTER JOIN authors ON books.author_id  = authors.id WHERE authors.name = 'Jenny James'"
 
     end
     it "with 1st arg :first, conditions for 'authors.name', joins :authors" do
-      expect(Books.find(:first, "authors.name = 'Jenny James'", :authors)).
+      expect(Books.
+        find(:first, conditions: "authors.name = 'Jenny James'", joins: [:authors])).
         to eq "SELECT * FROM books LEFT OUTER JOIN authors ON books.author_id  = authors.id WHERE authors.name = 'Jenny James' LIMIT 1"          
     end
   end
