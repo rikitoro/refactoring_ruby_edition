4e62f380b87dd6feaeb8e008c8b9f2bc57c20f1b
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-14 20:12:12 +0900

    first version of sec06_16

---
+++ sec06_16/product_count_item.rb
@@ -0,0 +1,11 @@
+class ProductCountItem
+  def initialize(search_criteria)
+    @search_criteria = search_criteria
+  end
+
+  def product_count_item(search_criteria = nil)
+    criteria = search_criteria || @search_criteria
+    ProductCountItem.find_all_by_criteria(criteria)
+  end
+  
+end
---
+++ sec06_16/product_count_item_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require_relative 'product_count_item'
+
+describe ProductCountItem do
+  describe '#product_count_items' do
+    it "with no arg" do
+      ProductCountItem.stub!(:find_all_by_criteria).and_return(:my_item)
+      item = ProductCountItem.new(:my_criteria)
+      expect(item.product_count_item).to eq :my_item
+    end
+
+    it "with arg" do
+      ProductCountItem.stub!(:find_all_by_criteria).and_return(:my_item)
+      item = ProductCountItem.new(:my_criteria)
+      expect(item.product_count_item(:new_criteria)).to eq :my_item
+    end
+
+  end  
+end
