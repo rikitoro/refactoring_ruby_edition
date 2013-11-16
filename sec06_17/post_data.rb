a6e26a2dc1dcc0fa40ad84f26c42cbd170292f3f
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-16 16:35:12 +0900

    dynamic method definition by extend Hash

--- sec06_17/post_data.rb
+++ sec06_17/post_data.rb
@@ -1,12 +1,19 @@
-class PostData
-
-  def initialize(post_data)
-    (class << self; self; end).class_eval do
-      post_data.each_pair do |key, value|
-        define_method key.to_sym do
+class Hash
+  def to_module
+    hash = self
+    Module.new do
+      hash.each_pair do |key, value|
+        define_method key do
           value
         end
       end
     end
+  end
+end
+
+class PostData
+
+  def initialize(post_data)
+    self.extend post_data.to_module
   end
 end
