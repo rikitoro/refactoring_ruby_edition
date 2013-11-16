599c7ec313ecf9f4f23e80d4322d82241398919a
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-16 18:07:42 +0900

    Replace dynamec receptor with dynamic method definition by class anotation

--- sec06_18/person.rb
+++ sec06_18/person.rb
@@ -1,13 +1,13 @@
 class Person
+  def self.attrs_with_empty_predicate(*args)
+    attr_accessor *args
 
-  attr_accessor :name, :age
-
-  def method_missing(sym, *args, &block)
-    empty?(sym.to_s.sub(/^empty_/, "").chomp("?"))
+    args.each do |attribute|
+      define_method "empty_#{attribute}?" do
+        self.send(attribute).nil?
+      end
+    end
   end
 
-  def empty?(sym)
-    self.send(sym).nil?
-  end
-
+  attrs_with_empty_predicate :name, :age
 end
