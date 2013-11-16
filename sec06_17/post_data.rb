53190af5d99a4bf4e4da3bd35c2576ac71601bc9
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-16 16:27:45 +0900

    dynamic method definition by class_eval

--- sec06_17/post_data.rb
+++ sec06_17/post_data.rb
@@ -1,13 +1,12 @@
 class PostData
+
   def initialize(post_data)
-    @post_data = post_data
-  end
-
-  def params
-    @post_data[:params]
-  end
-
-  def session
-    @post_data[:session]
+    (class << self; self; end).class_eval do
+      post_data.each_pair do |key, value|
+        define_method key.to_sym do
+          value
+        end
+      end
+    end
   end
 end
