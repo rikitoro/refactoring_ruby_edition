8b232b0ea7259a79652d5a66bd1fa4be30cdf6ac
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-17 21:47:18 +0900

    move Account#overdraft_charge to AccountType#overdraft_charge

--- sec07_01/account.rb
+++ sec07_01/account.rb
@@ -5,19 +5,11 @@ class Account
     @days_overdrawn = days_overdrawn
   end
   
-  def overdraft_charge
-    if @account_type.premium?
-      result = 10
-      result += (@days_overdrawn - 7) * 0.85 if @days_overdrawn > 7
-      result
-    else
-      @days_overdrawn * 1.75
-    end
-  end
-
   def bank_charge
     result = 4.5
-    result += overdraft_charge if @days_overdrawn > 0
+    if @days_overdrawn > 0
+      result += @account_type.overdraft_charge(@days_overdrawn)
+    end
     result
   end
   
--- sec07_01/account_type.rb
+++ sec07_01/account_type.rb
@@ -6,4 +6,15 @@ class AccountType
   def premium?
     @type == :premium
   end
+
+  def overdraft_charge(days_overdrawn)
+    if premium?
+      result = 10
+      result += (days_overdrawn - 7) * 0.85 if days_overdrawn > 7
+      result
+    else
+      days_overdrawn * 1.75
+    end
+  end
+  
 end
