f974d0a215fcf291ddf7901bceeba8962a3f89cc
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-21 11:06:11 +0900

    move field: move Account.interest_rate to AccountType.interest_rate

--- sec07_02/account.rb
+++ sec07_02/account.rb
@@ -1,9 +1,17 @@
-class Account
+class AccountType
+  attr_accessor :interest_rate
+
   def initialize(interest_rate)
     @interest_rate = interest_rate
   end
+end
+
+class Account
+  def initialize(account_type)
+    @account_type = account_type
+  end
 
   def interest_for_amount_days(amount, days)
-    @interest_rate * amount * days / 365
+    @account_type.interest_rate * amount * days / 365
   end
 end
