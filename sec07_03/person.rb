a319222490640fe0ad57a3c99818b7f63d663dc5
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-21 19:45:36 +0900

    extract Person class

--- sec07_03/person.rb
+++ sec07_03/person.rb
@@ -1,15 +1,31 @@
 class Person
 
   attr_reader :name
-  attr_accessor :office_area_code
   attr_accessor :office_number
-  
+
   def initialize(name, office_area_code, office_number)
     @name = name
-    @office_area_code, @office_number = office_area_code, office_number
+    @office_number = office_number
+    @office_telephone = TelephoneNumber.new(office_area_code, office_number)
   end
   
   def telephone_number
-    '(' + @office_area_code + ') ' + @office_number
+    '(' + office_area_code + ') ' + office_number
+  end
+
+  def office_area_code
+    @office_telephone.area_code
+  end
+
+  def office_number
+    @office_telephone.number
+  end
+end
+
+class TelephoneNumber
+  attr_accessor :area_code, :number
+  def initialize(area_code, number)
+    @area_code = area_code
+    @number = number
   end
 end
