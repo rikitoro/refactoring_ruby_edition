5a94680ea04346bb1564307632046c9a82a88152
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-21 11:17:46 +0900

    move field Account.interest_rate using self encapsulate field

--- sec07_02/account.rb
+++ sec07_02/account.rb
@@ -1,9 +1,22 @@
-class Account
+class AccountType
+  attr_accessor :interest_rate
+
   def initialize(interest_rate)
-    @interest_rate = interest_rate
+    self.interest_rate = interest_rate   
+  end
+end
+
+class Account
+
+  def initialize(account_type)
+    @account_type = account_type
   end
 
   def interest_for_amount_days(amount, days)
-    @interest_rate * amount * days / 365
+    interest_rate * amount * days / 365
+  end
+
+  def interest_rate
+    @account_type.interest_rate
   end
 end
--- sec07_02/account_spec.rb
+++ sec07_02/account_spec.rb
@@ -2,8 +2,9 @@ require 'rspec'
 require_relative 'account'
 
 describe Account do 
-  context 'when interest rate is 0.1' do
-    let(:my_account) { Account.new(0.1) }
+  context 'when its account type is normal (interest rate is 0.1)' do
+    let(:normal_type) { AccountType.new(0.1) }
+    let(:my_account) { Account.new(normal_type) }
     describe '#interest_for_amount_days' do
       context 'with args: amount 100, days 365' do
         subject(:interest){ my_account.interest_for_amount_days(100, 365) }
