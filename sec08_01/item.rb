a0ab5c62ea60cc823b9fd00637bc0a15b6963cc0
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-23 15:06:26 +0900

    extract Item#setup

--- sec08_01/item.rb
+++ sec08_01/item.rb
@@ -2,9 +2,13 @@ class Item
   attr_accessor :base_price, :tax_rate
 
   def initialize(base_price, tax_rate)
+    setup(base_price, tax_rate)
+  end
+
+  def setup(base_price, tax_rate) 
     @base_price = base_price
     @tax_rate = tax_rate
-  end
+  end 
 
   def raise_base_price_by(persent)
     self.base_price = base_price * (1 + persent/100.0)
