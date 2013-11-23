8d32d169b460fafdeaf46ad3a0de899bad9c121f
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-23 15:04:51 +0900

    self encapsulate field: Item.base_price, Item.tax_rate

--- sec08_01/item.rb
+++ sec08_01/item.rb
@@ -1,14 +1,16 @@
 class Item
+  attr_accessor :base_price, :tax_rate
+
   def initialize(base_price, tax_rate)
     @base_price = base_price
     @tax_rate = tax_rate
   end
 
   def raise_base_price_by(persent)
-    @base_price = @base_price * (1 + persent/100.0)
+    self.base_price = base_price * (1 + persent/100.0)
   end
 
   def total
-    @base_price * (1 + @tax_rate)
+    base_price * (1 + tax_rate)
   end
 end
