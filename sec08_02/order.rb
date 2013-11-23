dfa3156cd65efe3f37e470a3bb27c6309abe06b4
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-23 17:33:34 +0900

    add Customer.create

--- sec08_02/order.rb
+++ sec08_02/order.rb
@@ -1,7 +1,7 @@
 class Order
 
   def initialize(customer_name)
-    @customer = Customer.new(customer_name)
+    @customer = Customer.create(customer_name)
   end
 
   def customer_name
@@ -9,14 +9,20 @@ class Order
   end
 
   def customer=(customer_name)
-    @customer = Customer.new(customer_name)
+    @customer = Customer.create(customer_name)
   end
 end
 
 class Customer
+
+  def self.create(name)
+    Customer.new(name)
+  end
+
   attr_reader :name
 
   def initialize(name)
     @name = name
   end
+
 end
