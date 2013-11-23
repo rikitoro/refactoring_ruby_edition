ea83ced80798fbe6cb37254839ed15eea636e8e2
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-23 17:20:13 +0900

    rename Order#customer to Order#customer_name

--- sec08_02/order.rb
+++ sec08_02/order.rb
@@ -1,15 +1,15 @@
 class Order
 
-  def initialize(customer)
-    @customer = Customer.new(customer)
+  def initialize(customer_name)
+    @customer = Customer.new(customer_name)
   end
 
-  def customer
+  def customer_name
     @customer.name
   end
 
-  def customer=(value)
-    @customer = Customer.new(value)
+  def customer=(customer_name)
+    @customer = Customer.new(customer_name)
   end
 end
 
--- sec08_02/order_spec.rb
+++ sec08_02/order_spec.rb
@@ -3,8 +3,9 @@ require_relative 'order'
 
 describe Order do
   context 'with customer Yamato' do
-    describe '#customer' do
-      it { expect(Order.new("Yamato").customer).to eq "Yamato"}
+    describe '#customer_name' do
+      it { expect(Order.new("Yamato").customer_name).to eq "Yamato"}
     end
   end
 end
+
