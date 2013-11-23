0c2e5d71b843cfb4ae5aa7fad3c9f65cda833ea8
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-11-23 17:17:38 +0900

    replace data value Order#customer with object Customer

--- sec08_02/order.rb
+++ sec08_02/order.rb
@@ -1,7 +1,22 @@
 class Order
-  attr_accessor :customer
 
   def initialize(customer)
-    @customer = customer
+    @customer = Customer.new(customer)
+  end
+
+  def customer
+    @customer.name
+  end
+
+  def customer=(value)
+    @customer = Customer.new(value)
+  end
+end
+
+class Customer
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
   end
 end
