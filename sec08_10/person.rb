ccb8c7419f504f2d82acd08fec6047a38631af56
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-12-29 18:43:28 +0900

    add Person#add_course, #remove_course, initialize_courses

--- sec08_10/person.rb
+++ sec08_10/person.rb
@@ -1,3 +1,19 @@
 class Person
   attr_accessor :courses
+  def initialize
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def remove_course(course)
+    @courses.delete(course)
+  end
+
+  def initialize_courses(courses)
+    raise "Courses should be empty" unless @courses.empty?
+    courses.each { |course| add_course(course) }
+  end
 end
