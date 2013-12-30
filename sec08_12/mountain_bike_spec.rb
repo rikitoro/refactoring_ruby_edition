e18b431a74332ffda5d5aaa25070d66122d7dd71
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-12-30 21:42:00 +0900

    呼び出し元からのタイプコードを削除

--- sec08_12/mountain_bike_spec.rb
+++ sec08_12/mountain_bike_spec.rb
@@ -5,8 +5,7 @@ require_relative 'full_suspension_mountain_bike'
 
 describe MountainBike do
   context 'for rigid type bike' do
-    let(:bike) { RigidMountainBike.new(type_code: :rigid,
-      tire_width: 2.5, commission: 0.1, base_price: 200) }
+    let(:bike) { RigidMountainBike.new(tire_width: 2.5, commission: 0.1, base_price: 200) }
     describe '#off_road_ability' do
       it { expect(bike.off_road_ability).to eq 2.5 * 0.5 }
     end
@@ -16,7 +15,7 @@ describe MountainBike do
   end
 
   context 'for front suspension type bike' do
-    let(:bike) { FrontSuspensionMountainBike.new(type_code: :front_suspension,
+    let(:bike) { FrontSuspensionMountainBike.new(
       tire_width: 2, front_fork_travel: 3, 
       commission: 0.1, base_price: 200, front_suspension_price: 20) }
     describe '#off_road_ability' do
@@ -28,7 +27,7 @@ describe MountainBike do
   end
 
   context 'for full suspension type bike' do
-    let(:bike) { FullSuspensionMountainBike.new(type_code: :full_suspension,
+    let(:bike) { FullSuspensionMountainBike.new(
       tire_width: 2, front_fork_travel: 3, rear_fork_travel: 3,
       commission: 0.1, base_price: 200, front_suspension_price: 20, rear_suspension_price: 15) }
     describe '#off_road_ability' do
