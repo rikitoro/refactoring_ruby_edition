9ed45e4d32c6f4e2b6afbf47c9333e900587641f
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-12-31 19:01:14 +0900

    module FullSuspensionMountainBike, FrontSuspensionMountainBikeで#off_road_abilityをオーバーライドした

--- sec08_13/full_suspension_mountain_bike.rb
+++ sec08_13/full_suspension_mountain_bike.rb
@@ -2,4 +2,12 @@ module FullSuspensionMountainBike
   def price
     (1 + @commission) * @base_price + @front_suspension_price + @rear_suspension_price
   end
+
+  def off_road_ability
+    @tire_width * MountainBike::TIRE_WIDTH_FACTOR + 
+    @front_fork_travel * MountainBike::FRONT_SUSPENSION_FACTOR + 
+    @rear_fork_travel * MountainBike::REAR_SUSPENSION_FACTOR
+  end
+
 end
+
