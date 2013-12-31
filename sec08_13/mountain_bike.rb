72848ddff5fbe53e2efc674c4794bb67fffe53a5
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2013-12-31 18:28:24 +0900

    MountainBikeクラスの@type_codeに自己カプセル化フィールドを適用

--- sec08_13/mountain_bike.rb
+++ sec08_13/mountain_bike.rb
@@ -3,10 +3,10 @@ class MountainBike
   FRONT_SUSPENSION_FACTOR = 0.3
   REAR_SUSPENSION_FACTOR = 0.2
 
-  attr_writer :type_code
+  attr_reader :type_code
 
   def initialize(params)
-    @type_code = params[:type_code]
+    self.type_code = params[:type_code]
     @commission = params[:commission]
     @front_fork_travel = params[:front_fork_travel]
     @rear_fork_travel = params[:rear_fork_travel]
@@ -16,19 +16,23 @@ class MountainBike
     @rear_suspension_price = params[:rear_suspension_price]
   end
 
+  def type_code=(value)
+    @type_code = value
+  end
+
   def off_road_ability
     result = @tire_width * TIRE_WIDTH_FACTOR
-    if @type_code == :front_suspension || @type_code == :full_suspension
+    if type_code == :front_suspension || type_code == :full_suspension
       result += @front_fork_travel * FRONT_SUSPENSION_FACTOR
     end
-    if @type_code == :full_suspension
+    if type_code == :full_suspension
       result += @rear_fork_travel * REAR_SUSPENSION_FACTOR
     end
     result
   end
 
   def price
-    case @type_code
+    case type_code
     when :rigid
       (1 + @commission) * @base_price
     when :front_suspension
